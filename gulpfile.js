9762cc18ca7819540f6fd84f838521850c348155
Dabney Blum <dabneyblum@gmail.com>
2014-12-06 14:19:56 -0600

    added more gulp tasks; pushing files to build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,57 @@
 var gulp = require('gulp'),
-   uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  minifyCSS = require('gulp-minify-css'),
+  imagemin = require('gulp-imagemin');
 
-gulp.task('uglify', function () {
-   gulp.src('views/js/main.js')
+gulp.task('uglifypizza', function () {
+   gulp.src('views/js/*.js')
       .pipe(uglify())
-      .pipe(gulp.dest('views/js/main.min.js'))
+      .pipe(gulp.dest('build/views/js'))
+});
+gulp.task('minifypizza', function () {
+   gulp.src('views/css/*.css')
+      .pipe(minifyCSS())
+      .pipe(gulp.dest('build/views/css'))
+});
+gulp.task('uglifyprimary', function () {
+   gulp.src('js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('build/js'))
+});
+gulp.task('minifyprimary', function () {
+   gulp.src('css/*.css')
+      .pipe(minifyCSS())
+      .pipe(gulp.dest('build/css'))
+});
+
+// I already used Photoshop to reduce the images but will push them through imagemin anyway
+gulp.task('imageminprimary', function () {
+   gulp.src(['img/2048.png', 'img/mobilewebdev.jpg', 'img/cam_be_like.jpg', 'img/profilepic.jpg'])
+      .pipe(imagemin())
+      .pipe(gulp.dest('build/img'))
+});
+
+// Push the pizza page images through imagemin.  I already used Photoshop to reduce these images
+//  and to create the pizzeria_as_icon image
+gulp.task('imageminpizza', function () {
+   gulp.src(['views/images/pizzeria_as_icon.jpg', 'views/images/pizza.png', 'views/images/pizzeria.jpg'])
+      .pipe(imagemin())
+      .pipe(gulp.dest('build/views/images'))
+});
+
+// Process the HTML files in the primary directory and put them in the build directory
+// Currently no processing is done but may add prettify task in the future
+gulp.task('processhtmlprimary', function () {
+   gulp.src('*.html')
+      .pipe(gulp.dest('build'))
+});
+
+// Process the HTML files in the pizza (views) directory and put them in the build directory
+// Currently no processing is done but may add prettify task in the future
+gulp.task('processhtmlpizza', function () {
+   gulp.src('views/pizza.html')
+      .pipe(gulp.dest('build/views'))
 });
 /*
 var gulp = require('gulp'),
